9ff3fc2de7989c54ce64007eb5d091e8cbe3fa0c
layzduckling <layzduckling@icloud.com>
2023-01-04 15:56:21 +0900

    Move result message feature to a separate func.
    main.js > playRound() -> createResultMessage()
    Change playRound() return values to "tie", "won" and "lost".

--- main.js
+++ main.js
@@ -8,8 +8,16 @@ function getComputerChoice() {
         return "paper";
     } else if (choiceNumber === 2) {
         return "scissors";
+    }
+}
+
+function createResultMessage(result, playerSelection, computerSelection) {
+    if (result === "won") {
+        return `You win! (${playerSelection} beats ${computerSelection})`;
+    } else if (result === "lost") {
+        return `You lose! (${computerSelection} beats ${playerSelection})`;
     } else {
-        return null;
+        return "It's a tie!";
     }
 }
 
@@ -17,20 +25,18 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase(); // make case-insensitive
 
     if (playerSelection === computerSelection) {
-        return "It's a tie!";
+        return "tie"
     } else if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")
     ) {
-        return `You win! (${playerSelection} beats ${computerSelection})`;
+        return "won"
     } else if (
         (playerSelection === "rock" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "scissors") ||
         (playerSelection === "scissors" && computerSelection === "rock")
     ) {
-        return `You lose! (${computerSelection} beats ${playerSelection})`;
-    } else {
-        return "Something went wrong...";
+        return "lost"
     }
 }
