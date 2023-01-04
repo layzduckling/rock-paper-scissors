7b75ce82d642946cbab982103a4b5b815c72f2b6
layzduckling <layzduckling@icloud.com>
2023-01-04 15:46:55 +0900

    Create a function that plays a single round.
    main.js > playRound()
    Play a single round of rock paper scissors.

--- main.js
+++ main.js
@@ -12,3 +12,25 @@ function getComputerChoice() {
         return null;
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase(); // make case-insensitive
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        return `You win! (${playerSelection} beats ${computerSelection})`;
+    } else if (
+        (playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "rock")
+    ) {
+        return `You lose! (${computerSelection} beats ${playerSelection})`;
+    } else {
+        return "Something went wrong...";
+    }
+}
